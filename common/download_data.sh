7e943f7bf6373a0a6df06cc3a21604d4d028d5df
Timothy Leong <timothy.leong@u.nus.edu>
2021-10-10 17:20:15 +0800

    Add file to download data

---
+++ common/download_data.sh
@@ -0,0 +1,12 @@
+########################################
+# Script to download project data files
+########################################
+
+# Get the zip file
+wget http://www.comp.nus.edu.sg/~cs4224/project_files_3.zip
+
+# Unzip into a folder
+unzip project_files_3
+
+# Delete the zip file
+rm project_files_3.zip
