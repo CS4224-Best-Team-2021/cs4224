6f3a9ed445a461d962f6a7dbd4c9cd5d97464c73
Timothy Leong <timothy.leong@u.nus.edu>
2021-10-14 12:54:50 +0800

    resolve path error in download_data.sh

--- common/scripts/download_data.sh
+++ common/scripts/download_data.sh
@@ -2,14 +2,17 @@
 # Script to download project data files #
 #########################################
 
+# Get the location of this script
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Remove the current data 
-rm -rf project_files_3
+rm -rf $SCRIPT_DIR/project_files_4
 
 # Get the zip file
 wget http://www.comp.nus.edu.sg/~cs4224/project_files_4.zip
 
 # Unzip into a folder
-unzip project_files_4.zip -d ..
+unzip project_files_4.zip -d $SCRIPT_DIR/..
 
 # Delete the zip file
 rm project_files_4.zip
