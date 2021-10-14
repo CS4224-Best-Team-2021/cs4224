bb2830a5d519e789d32422cbd1c998296f41b1dc
Andy <andy.lam@u.nus.edu>
2021-10-14 10:51:32 +0800

    Stock level transaction

---
+++ cockroach/transactions/stock_level.py
@@ -0,0 +1,38 @@
+import psycopg2
+import logging
+
+
+def stock_level_transaction(
+    conn, warehouse_num, district_num, stock_threshold, num_last_orders_to_examine
+):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            SELECT COUNT(*)
+            FROM 
+                Stock AS s
+            WHERE 
+                s.S_QUANTITY < %s
+                AND s.S_I_ID IN 
+                    (SELECT OL_I_ID AS item_number
+                    FROM 
+                        Order AS o 
+                        INNER JOIN 
+                        Order-Line AS ol
+                    ON 
+                        o.O_W_ID = ol.OL_W_ID
+                        AND o.O_D_ID = ol.OL_D_ID
+                        AND o.O_ID = ol.OL_O_ID
+                    WHERE
+                        o.O_W_ID = %s 
+                        AND o.O_D_ID = %s
+                    ORDER BY 
+                        O_ENTRY_D DESC
+                    LIMIT 
+                        %s);
+            """,
+            (stock_threshold, warehouse_num, district_num, num_last_orders_to_examine),
+        )
+        logging.debug(f"stock_level_transaction(): Status Message {cur.statusmessage}")
+
+    conn.commit()
