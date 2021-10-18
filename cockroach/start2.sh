8897ae975da4daac4e888d42883a1825c1559d14
Timothy Leong <timothy.leong@u.nus.edu>
2021-10-19 07:56:49 +0800

    add gitignore and edit start script for secure startup

--- cockroach/start2.sh
+++ cockroach/start2.sh
@@ -5,7 +5,7 @@ SERVER3="192.168.48.171"
 SERVER4="192.168.48.172" 
 SERVER5="192.168.48.173"
 
-# Create a hash table for LAN address and ports
+# Set LAN address and ports
 declare -A ports
 ports[$SERVER1]=5001
 ports[$SERVER2]=5002
@@ -13,7 +13,7 @@ ports[$SERVER3]=5003
 ports[$SERVER4]=5004
 ports[$SERVER5]=5005
 
-# Create a hash table for the store names of each db
+# Set the store names of each db
 declare -A storenames
 storenames[$SERVER1]="store1" 
 storenames[$SERVER2]="store2" 
@@ -21,7 +21,7 @@ storenames[$SERVER3]="store3"
 storenames[$SERVER4]="store4" 
 storenames[$SERVER5]="store5"
 
-# Create a hash table for the http addresses
+# Set the http addresses
 declare -A https
 https[$SERVER1]=9001
 https[$SERVER2]=9002
@@ -29,23 +29,29 @@ https[$SERVER3]=9003
 https[$SERVER4]=9004
 https[$SERVER5]=9005
 
+# Set cert directories
+declare -A certs
+certs[$SERVER1]="certs1" 
+certs[$SERVER2]="certs2" 
+certs[$SERVER3]="certs3" 
+certs[$SERVER4]="certs4" 
+certs[$SERVER5]="certs5"
 
 # Get the specific variables of this machine
 HOST_NAME="$( hostname -i )"
 PORT=${ports[$HOST_NAME]}
 STORE=${storenames[$HOST_NAME]}
 HTTP_PORT=${https[$HOST_NAME]}
+CERTS=${certs[$HOST_NAME]}
 
 # Confirm to the user which IP address and port this machine will advertise and listen on
 echo "This machine will advertise and listen on the address: $HOST_NAME:$PORT"
 
-# Ask if cluster has been initialised
-# read -p "Type 'yes' if you need to run cockroach init, or hit the Enter key otherwise: " response
-
 # NOTE: You must use the same port configurations on all servers.
 cockroach start \
---insecure \
+--certs-dir=$CERTS \
 --listen-addr=$HOST_NAME:$PORT \
+--advertise-addr=$HOST_NAME:$PORT \
 --join=$SERVER1:$PORT_1,$SERVER2:$PORT_2,$SERVER3:$PORT_3,$SERVER4:$PORT_4,$SERVER5:$PORT_5 \
 --cache=.25 \
 --max-sql-memory=.25 \
@@ -53,7 +59,3 @@ cockroach start \
 --store=$STORE \
 --http-addr=$HOST_NAME:$HTTP_PORT
 
-# if response='yes' 
-# then 
-#     cockroach init --insecure --host=$HOST_NAME:$PORT
-# fi
