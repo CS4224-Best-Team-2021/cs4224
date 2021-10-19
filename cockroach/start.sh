24a3cde18a28f57ca17ef219f5f415e8626cfabb
Timothy Leong <timothy.leong@u.nus.edu>
2021-10-19 10:59:39 +0800

    replace the start script

--- cockroach/start.sh
+++ cockroach/start.sh
@@ -1,56 +1,26 @@
-# Get ports of every server
-read -p "xcnc20 port number: " PORT_1
-read -p "xcnc21 port number: " PORT_2
-read -p "xcnc22 port number: " PORT_3
-read -p "xcnc23 port number: " PORT_4
-read -p "xcnc24 port number: " PORT_5
-
-# Create aliases for all servers
-SERVER1="192.168.48.169" 
-SERVER2="192.168.48.170" 
-SERVER3="192.168.48.171" 
-SERVER4="192.168.48.172" 
-SERVER5="192.168.48.173"
-
-# Create a hash table for LAN address and ports
-declare -A ports
-ports[$SERVER1]=$PORT_1 
-ports[$SERVER2]=$PORT_2 
-ports[$SERVER3]=$PORT_3 
-ports[$SERVER4]=$PORT_4 
-ports[$SERVER5]=$PORT_5
-
-# Create a hash table for the store names of each db
-declare -A storenames
-storenames[$SERVER1]="store1" 
-storenames[$SERVER2]="store2" 
-storenames[$SERVER3]="store3" 
-storenames[$SERVER4]="store4" 
-storenames[$SERVER5]="store5"
-
+# Read in the configs
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $SCRIPT_DIR/config.sh
 
 # Get the specific variables of this machine
 HOST_NAME="$( hostname -i )"
 PORT=${ports[$HOST_NAME]}
 STORE=${storenames[$HOST_NAME]}
+HTTP_PORT=${https[$HOST_NAME]}
+CERTS=${certs[$HOST_NAME]}
 
 # Confirm to the user which IP address and port this machine will advertise and listen on
 echo "This machine will advertise and listen on the address: $HOST_NAME:$PORT"
 
-# Ask if cluster has been initialised
-read -p "Type 'yes' if you need to run cockroach init, or hit the Enter key otherwise: " response
-
-# NOTE: You must use the same port configurations on all servers.
+# Start the database
 cockroach start \
---insecure \
+--certs-dir=$SCRIPT_DIR/$CERTS \
 --listen-addr=$HOST_NAME:$PORT \
+--advertise-addr=$HOST_NAME:$PORT \
 --join=$SERVER1:$PORT_1,$SERVER2:$PORT_2,$SERVER3:$PORT_3,$SERVER4:$PORT_4,$SERVER5:$PORT_5 \
 --cache=.25 \
 --max-sql-memory=.25 \
 --background \
---store=$STORE
+--store=$SCRIPT_DIR/$STORE \
+--http-addr=$HOST_NAME:$HTTP_PORT
 
-if response='yes' 
-then 
-    cockroach init --insecure --host=$HOST_NAME:$PORT
-fi
