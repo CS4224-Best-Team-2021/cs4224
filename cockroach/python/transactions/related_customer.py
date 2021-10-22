abf04695109501c3e9e5839482473ef5ca9d1dc2
Andy <andy.lam@u.nus.edu>
2021-10-22 11:25:24 +0800

    Add view for related custoemr

--- cockroach/python/transactions/related_customer.py
+++ cockroach/python/transactions/related_customer.py
@@ -8,57 +8,15 @@ def related_customer_transaction(conn, customer):
     with conn.cursor() as cur:
         cur.execute(
             """
+            REFRESH MATERIALIZED VIEW related_customer_view;
             SELECT 
-                c.C_W_ID,
-                c.C_D_ID,
-                c.C_ID
-            FROM   customer AS c
-            WHERE  c.C_W_ID != %s EXISTS
-                (
-                        SELECT 1
-                        FROM   ORDER AS o1
-                        WHERE  o1.O_W_ID = %s
-                        AND    o1.O_D_ID = %s
-                        AND    o1.O_C_ID = %s
-                        AND    EXISTS
-                                (
-                                        SELECT 1
-                                        FROM   ORDER AS o2
-                                        WHERE  o1.O_W_ID = c.C_W_ID
-                                        AND    o1.O_D_ID = c.C_D_ID
-                                        AND    o1.O_C_ID = c.C_ID
-                                        AND    EXISTS
-                                            (
-                                                    SELECT 1
-                                                    FROM   ORDER-line AS ol1
-                                                    WHERE  ol1.OL_W_ID = o1.O_W_ID
-                                                    AND    ol1.OL_D_ID = o1.O_D_ID
-                                                    AND    ol1.OL_O_ID = o1.OL_O_ID
-                                                    AND    EXISTS
-                                                            (
-                                                                    SELECT 1
-                                                                    FROM   ORDER-line AS ol2
-                                                                    WHERE  ol2.OL_W_ID = o1.O_W_ID
-                                                                    AND ol1.OL_D_ID = o1.O_D_ID
-                                                                    AND ol2.OL_O_ID = o1.OL_O_ID
-                                                                    AND    EXISTS
-                                                                        (
-                                                                                SELECT 1
-                                                                                FROM   ORDER-line AS ol3
-                                                                                WHERE  ol3.OL_W_ID = o2.O_W_ID
-                                                                                AND    ol1.OL_D_ID = o2.O_D_ID
-                                                                                AND    ol3.OL_O_ID = o2.OL_O_ID
-                                                                                AND    EXISTS
-                                                                                        (
-                                                                                                SELECT 1
-                                                                                                FROM   ORDER-line AS ol4
-                                                                                                WHERE  ol4.OL_W_ID = o2.O_W_ID
-                                                                                                AND    ol1.OL_D_ID = o2.O_D_ID
-                                                                                                AND    ol4.OL_O_ID = o2.OL_O_ID
-                                                                                                AND    ol1.OL_I_ID != ol2.OL_I_ID
-                                                                                                AND    ol3.OL_I_ID != ol4.OL_I_ID
-                                                                                                AND    ol1.OL_I_ID = ol3.OL_I_ID
-                                                                                                AND    ol2.OL_I_ID = ol4.OL_I_ID ) ) ) ) ) );
+                C_W_ID2, C_D_ID2, C_ID2
+            FROM 
+                related_customer_view
+            WHERE
+                C_W_ID1 = %s
+                AND C_D_ID1 = %s
+                AND C_ID1 = %s;
             """,
             (c_w_id, c_w_id, c_d_id, c_id),
         )
