3ee5d689a805a0c7e12511c231ceb1faf3851ba1
cs4224b <cs4224b@xcnc20.comp.nus.edu.sg>
2021-10-23 20:24:32 +0800

    Fix schema sql

--- cockroach/schema/init_db_b.sql
+++ cockroach/schema/init_db_b.sql
@@ -164,27 +164,27 @@ SELECT DISTINCT
 FROM
     customer AS c1,
     customer AS c2,
-    order AS o1,
-    order AS o2,
+    "order" AS o1,
+    "order" AS o2,
     order_line AS ol1,
     order_line AS ol2,
     order_line AS ol3,
     order_line AS ol4
-ON 
+WHERE 
     -- c1 and c2 in different warehouses
     c1.C_W_ID != c2.C_W_ID
     -- c1 has o1
-    c1.C_W_ID = o1.O_W_ID
+    AND c1.C_W_ID = o1.O_W_ID
     AND c1.C_D_ID = o1.O_D_ID
     AND c1.C_ID = o1.O_C_ID
     -- c2 has o2
-    c2.C_W_ID = o2.O_W_ID
+    AND c2.C_W_ID = o2.O_W_ID
     AND c2.C_D_ID = o2.O_D_ID
     AND c2.C_ID = o2.O_C_ID
     -- ol1 in o1
     AND  ol1.OL_W_ID = o1.O_W_ID
     AND  ol1.OL_D_ID = o1.O_D_ID
-    AND  ol1.OL_O_ID = o1.OL_O_ID
+    AND  ol1.OL_O_ID = o1.O_ID
     -- OL1 and OL2 are in o1
     AND ol1.OL_W_ID = ol2.OL_W_ID 
     AND ol1.OL_D_ID = ol2.OL_D_ID
@@ -192,11 +192,11 @@ ON
     -- OL3 and OL4 are in the same order
     AND ol3.OL_W_ID = ol4.OL_W_ID
     AND ol3.OL_D_ID = ol4.OL_D_ID
-    AND ol3.OL_O_ID = ol4.OL_O_ID;
+    AND ol3.OL_O_ID = ol4.OL_O_ID
     -- ol3 and ol4 are in o2
     AND  ol3.OL_W_ID = o2.O_W_ID
     AND  ol3.OL_D_ID = o2.O_D_ID
-    AND  ol3.OL_O_ID = o2.OL_O_ID
+    AND  ol3.OL_O_ID = o2.O_ID
     -- OL1 and OL2 are not the same items
     AND ol1.OL_I_ID != ol2.OL_I_ID 
     -- OL3 and OL4 are not the same items
