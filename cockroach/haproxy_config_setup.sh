6273601b4ac4886a5f294387d7168d5430f9c19a
Timothy Leong <timothy.leong@u.nus.edu>
2021-11-01 12:13:21 +0800

    replace the default port in load balancer

--- cockroach/haproxy_config_setup.sh
+++ cockroach/haproxy_config_setup.sh
@@ -4,8 +4,10 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/config.sh
 
-
+# Generate the config file for HAProxy
 cockroach gen haproxy \
 --certs-dir=$SCRIPT_DIR/root-cert \
---host=$SERVER1:${ports[$SERVER1]} \
---port=7000
+--host=$SERVER1:${ports[$SERVER1]} 
+
+# Replace the default port of the load balancer with some unused number, e.g. 7000
+sed -i 's/26257/7000' haproxy.cfg
