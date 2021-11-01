23aa8b9a31dd72d2bd9fa1f66d547c42083389e2
Timothy Leong <timothy.leong@u.nus.edu>
2021-11-01 16:06:56 +0800

    change dsn to point to load balancer

--- cockroach/app_experimental.sh
+++
@@ -1,1 +0,0 @@
-python3 python/main.py "postgresql://root@192.168.48.169:7000/wholesaledb?sslcert=root-cert/client.root.crt&sslkey=root-cert/client.root.key&sslmode=verify-ca&sslrootcert=root-cert/ca.crt" 1
