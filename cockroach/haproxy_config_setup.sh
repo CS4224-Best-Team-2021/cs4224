432dbf764a97940e47ca0e2536c1c0cea5829e68
Timothy Leong <timothy.leong@u.nus.edu>
2021-11-01 16:52:52 +0800

    allow the load balancer to be run on the current server

--- cockroach/haproxy_config_setup.sh
+++ cockroach/haproxy_config_setup.sh
@@ -4,10 +4,13 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/config.sh
 
+HOST_NAME="$( hostname -i )"
+PORT=${ports[$HOST_NAME]}
+
 # Generate the config file for HAProxy
 cockroach gen haproxy \
 --certs-dir=$SCRIPT_DIR/root-cert \
---host=$SERVER1:${ports[$SERVER1]} 
+--host=$HOST_NAME:$PORT 
 
 # Replace the default port of the load balancer with some unused number, e.g. 7000
 sed -i 's/26257/7000/g' haproxy.cfg
