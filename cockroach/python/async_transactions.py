ee0d1f5430ac6678fa69070b22729e98edbe0a6b
Timothy Leong <timothy.leong@u.nus.edu>
2021-11-01 23:34:36 +0800

    add async transaction runner

---
+++ cockroach/python/async_transactions.py
@@ -0,0 +1,24 @@
+import asyncio
+
+async def run(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    stdout, stderr = await proc.communicate()
+
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        print(f'[stdout]\n{stdout.decode()}')
+    if stderr:
+        print(f'[stderr]\n{stderr.decode()}')
+
+async def main():
+    txns = []
+    for i in range(0, 1):
+        txns.append(run(f'cat ~/temp/cs4224/common/project_files_4/xact_files_A/0.txt'))
+
+    await asyncio.gather(*txns)
+
+asyncio.run(main())
