263bbcffe61514d7bfccffbef6e99aca8979f8f1
Timothy Leong <timothy.leong@u.nus.edu>
2021-11-01 16:57:06 +0800

    allow create db to be run on any server

--- cockroach/create-db.sh
+++ cockroach/create-db.sh
@@ -9,21 +9,24 @@ done
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/config.sh
 
+HOST_NAME="$( hostname -i )"
+PORT=${ports[$HOST_NAME]}
+
 # Creates an empty database using init_db_a.sql or init_db_b.sql 
 if [[ $workload == "a" ]]
 then
-    cockroach sql -f=$SCRIPT_DIR/schema/init_db_a.sql --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
+    cockroach sql -f=$SCRIPT_DIR/schema/init_db_a.sql --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
 else
-    cockroach sql -f=$SCRIPT_DIR/schema/init_db_b.sql --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
+    cockroach sql -f=$SCRIPT_DIR/schema/init_db_b.sql --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
 fi
 
 # Import each CSV file into the database
-cockroach sql -e="use wholesaledb; IMPORT INTO warehouse CSV DATA ('nodelocal://self/warehouse.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
-cockroach sql -e="use wholesaledb; IMPORT INTO district CSV DATA ('nodelocal://self/district.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
-cockroach sql -e="use wholesaledb; IMPORT INTO customer CSV DATA ('nodelocal://self/customer.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
-cockroach sql -e="use wholesaledb; IMPORT INTO wholesaledb.order CSV DATA ('nodelocal://self/order.csv') WITH nullif = 'null';" --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
-cockroach sql -e="use wholesaledb; IMPORT INTO item CSV DATA ('nodelocal://self/item.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
-cockroach sql -e="use wholesaledb; IMPORT INTO order_line CSV DATA ('nodelocal://self/order-line.csv') WITH nullif = 'null';" --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
-cockroach sql -e="use wholesaledb; IMPORT INTO stock CSV DATA ('nodelocal://self/stock.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
+cockroach sql -e="use wholesaledb; IMPORT INTO warehouse CSV DATA ('nodelocal://self/warehouse.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
+cockroach sql -e="use wholesaledb; IMPORT INTO district CSV DATA ('nodelocal://self/district.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
+cockroach sql -e="use wholesaledb; IMPORT INTO customer CSV DATA ('nodelocal://self/customer.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
+cockroach sql -e="use wholesaledb; IMPORT INTO wholesaledb.order CSV DATA ('nodelocal://self/order.csv') WITH nullif = 'null';" --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
+cockroach sql -e="use wholesaledb; IMPORT INTO item CSV DATA ('nodelocal://self/item.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
+cockroach sql -e="use wholesaledb; IMPORT INTO order_line CSV DATA ('nodelocal://self/order-line.csv') WITH nullif = 'null';" --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
+cockroach sql -e="use wholesaledb; IMPORT INTO stock CSV DATA ('nodelocal://self/stock.csv');" --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
 
-cockroach sql -f=$SCRIPT_DIR/schema/views.sql --certs-dir=$SCRIPT_DIR/root-cert --host=$SERVER1:${ports[$SERVER1]}
+cockroach sql -f=$SCRIPT_DIR/schema/views.sql --certs-dir=$SCRIPT_DIR/root-cert --host=$HOST_NAME:$PORT
