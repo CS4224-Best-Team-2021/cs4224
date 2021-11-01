688fd0617843e7749ee35036eb3c74507f006648
Timothy Leong <timothy.leong@u.nus.edu>
2021-11-01 18:18:22 +0800

    make cp recursive

--- cockroach/make_certs_exe.sh
+++ cockroach/make_certs_exe.sh
@@ -2,8 +2,8 @@
 
 for i in {1..5}
 do 
-    cp certs$i certs${i}temp # copy the folder to a temp folder
+    cp -r certs$i certs${i}temp # copy the folder to a temp folder
     rm -rf certs$i # remove the original
-    cp certs${i}temp certs$i # copy the temp into the original
+    cp -r certs${i}temp certs$i # copy the temp into the original
     rm -rf certs${i}temp # remove the temp folder
 done
