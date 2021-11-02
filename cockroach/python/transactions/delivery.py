1a909404664dfe1dd6d60d6ec4bc1c118831eaeb
Timothy Leong <timothy.leong@u.nus.edu>
2021-11-02 17:10:57 +0800

    try different method

--- cockroach/python/transactions/delivery.py
+++ cockroach/python/transactions/delivery.py
@@ -5,6 +5,11 @@ def delivery_transaction(conn, log_buffer, test, w_id, carrier_id):
     No output required for this transaction
     """
     for district_no in range(1, 11):
+        deliver_to_one_district(conn, w_id, carrier_id, district_no)
+
+
+def deliver_to_one_district(conn, w_id, carrier_id, d_id):
+    with conn.cursor() as cur:
         N = 0
         
         # (a) Find the earliest unfulfilled order for this warehouse and district
@@ -26,17 +31,16 @@ def delivery_transaction(conn, log_buffer, test, w_id, carrier_id):
                     )
                 FOR UPDATE;
                 """,
-                (w_id, district_no),
+                (w_id, d_id),
             ) # uses customer_order index
 
             result = cur.fetchone()
 
-            # If there is no unfulfilled order, go to the next district
+            # If there is no unfulfilled order, return early
             if result is None:
-                continue
-
+                return
+            
             N = result[0]
-
 
         # (b) Assign this order to the given carrier
         with conn.cursor() as cur:   
@@ -49,10 +53,9 @@ def delivery_transaction(conn, log_buffer, test, w_id, carrier_id):
                 WHERE 
                     (O_W_ID, O_D_ID, O_ID) = (%s, %s, %s);
                 """,
-                (carrier_id, w_id, district_no, N),
+                (carrier_id, w_id, d_id, N),
             ) # uses primary key index
         
-
         # (c) Update all order-lines in this order
         with conn.cursor() as cur:
             cur.execute(
@@ -65,9 +68,8 @@ def delivery_transaction(conn, log_buffer, test, w_id, carrier_id):
                 WHERE
                     (OL_W_ID, OL_D_ID, OL_O_ID) = (%s, %s, %s);
                 """,
-                (w_id, district_no, N),
+                (w_id, d_id, N),
             ) # uses order_index
-        
 
         # (d) Update the customer
         O_C_ID = 0 
@@ -83,7 +85,7 @@ def delivery_transaction(conn, log_buffer, test, w_id, carrier_id):
                 WHERE 
                     (O_W_ID, O_D_ID, O_ID) = (%s, %s, %s);
                 """,
-                (w_id, district_no, N),
+                (w_id, d_id, N),
             ) # uses primary key index
 
             result = cur.fetchone()
@@ -101,7 +103,7 @@ def delivery_transaction(conn, log_buffer, test, w_id, carrier_id):
                 WHERE
                     (OL_W_ID, OL_D_ID, OL_O_ID) = (%s, %s, %s);
                 """,
-                (w_id, district_no, N),
+                (w_id, d_id, N),
             ) # uses order_index
 
             result = cur.fetchone()
@@ -119,8 +121,7 @@ def delivery_transaction(conn, log_buffer, test, w_id, carrier_id):
                 WHERE
                     (C_W_ID, C_D_ID, C_ID) = (%s, %s, %s);
                 """,
-                (B, w_id, district_no, O_C_ID),
+                (B, w_id, d_id, O_C_ID),
             ) # uses primary key index
-        
-        conn.commit() # Add more intermittent commits to prevent contention
 
+    conn.commit()
