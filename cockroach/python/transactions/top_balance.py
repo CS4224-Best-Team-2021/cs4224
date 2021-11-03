26e43cc40a1dd5547a8a098d23efe0303fd68273
Andy <andy.lam@u.nus.edu>
2021-11-03 15:29:44 +0800

    Remove views.sql

--- cockroach/python/transactions/top_balance.py
+++ cockroach/python/transactions/top_balance.py
@@ -10,14 +10,26 @@ def top_balance_transaction(conn, log_buffer, test):
     with conn.cursor() as cur:
         cur.execute(
             """
-            SELECT * FROM top_balance;
+            SELECT 
+                c.C_FIRST,
+                c.C_MIDDLE,
+                c.C_LAST,
+                c.C_BALANCE,
+                (SELECT W_NAME FROM Warehouse AS w WHERE w.W_ID = c.C_W_ID) AS Warehouse_Name,
+                (SELECT D_NAME FROM District AS d WHERE d.D_ID = c.C_D_ID AND d.D_W_ID = c.C_W_ID) AS District_Name
+            FROM 
+                Customer AS c
+            ORDER BY
+                c.C_BALANCE DESC
+            LIMIT 10;
             """
         )
         result = cur.fetchall()
-        log_buffer.append('In descending order of C_BALANCE (C_FIRST, C_MIDDLE, C_LAST, C_BALANCE, W_NAME, D_NAME):')
+        log_buffer.append(
+            "In descending order of C_BALANCE (C_FIRST, C_MIDDLE, C_LAST, C_BALANCE, W_NAME, D_NAME):"
+        )
         for i in result:
-            log_buffer.append(f'    {i}')
+            log_buffer.append(f"    {i}")
         logging.debug(f"top_balance_transaction(): Status Message {cur.statusmessage}")
 
     conn.commit()
-
